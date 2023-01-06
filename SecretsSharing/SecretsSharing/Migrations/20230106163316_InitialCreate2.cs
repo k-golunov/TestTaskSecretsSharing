9ed9feef9c9b29e1d7049423b7d453f167f99528
Kostya Golunov <kostya.golunov2015@yandex.ru>
2023-01-06 21:34:24 +0500

    Update database

---
+++ SecretsSharing/SecretsSharing/Migrations/20230106163316_InitialCreate2.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SecretsSharing.Migrations
+{
+    public partial class InitialCreate2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDelete",
+                table: "Files",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDelete",
+                table: "Files");
+        }
+    }
+}
